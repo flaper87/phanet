af99f58d908e729565e5ff1a2194d2ea9b487d72
incluye <jdtdj@fuse.net>
2010-01-15 17:38:47 -0500

    Dynamic. That wasn't hard.

--- themes/lstest.php
+++ themes/lstest.php
@@ -1,4 +1,5 @@
-<?php
+<?php // This file is not dynamic. It looks for a table on localhost called phanet wherein the theme table is called ph1_themes. Working with db settings to make it dynamic
+require('../settings.inc.php');
 $listOfThemes = array('');
 foreach (glob('*') as $file) {
 	if(is_dir($file)) {
@@ -8,12 +9,12 @@ foreach (glob('*') as $file) {
 
 $uninstalledThemes = array('');
 
-$link = mysql_connect('localhost', 'root', 'root');
+$link = mysql_connect($SETTINGS['db_server'], $SETTINGS['db_user'], $SETTINGS['db_password']);
 	if($link) {
-		if(mysql_select_db('phanet', $link)) {
+		if(mysql_select_db($SETTINGS['db_database'], $link)) {
 				foreach($listOfThemes as $installed) {
 					if(empty($installed)) { continue; }
-					$query = "SELECT * FROM `ph1_themes`
+					$query = "SELECT * FROM `".$SETTINGS['db_prefix']."themes`
 					WHERE `name` = '".$installed."'";
 				$results = mysql_query($query);
 				$results = mysql_fetch_array($results);
@@ -31,10 +32,10 @@ $link = mysql_connect('localhost', 'root', 'root');
 	}
 foreach($uninstalledThemes as $addNewTheme) {
 	if(empty($addNewTheme)) { continue; }
-	$link = mysql_connect('localhost', 'root', 'root');
+	$link = mysql_connect($SETTINGS['db_server'], $SETTINGS['db_user'], $SETTINGS['db_password']);
 		if($link) {
-			if(mysql_select_db('phanet', $link)) {
-				$query = "INSERT INTO  `phanet`.`ph1_themes` (
+			if(mysql_select_db($SETTINGS['db_database'], $link)) {
+				$query = "INSERT INTO  `".$SETTINGS['db_prefix']."themes` (
 				`id` ,
 				`name` ,
 				`path` ,
