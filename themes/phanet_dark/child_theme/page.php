9e089c8210ad10da5efc4d24c93e9638c1806544
incluye <jdtdj@fuse.net>
2010-01-16 15:31:22 -0500

    See the glory that is an understandable page.php!

--- themes/phanet_dark/child_theme/page.php
+++ themes/phanet_dark/child_theme/page.php
@@ -1,14 +1,13 @@
 <div class="single_post">
 <div class="title_date">
-<p class="days"><?php echo get_date('j',$page->page_date); ?></p>
-<p class="month"><?php echo get_date('M',$page->page_date); ?></p>
+<p class="days"><?php pageDate('j'); ?></p>
+<p class="month"><?php pageDate('m'); ?></p>
 </div>
 <div class="title">
-<h4><?php echo $page->page_title; ?></h4>
+<h4><?php pageTitle(); ?></h4>
 </div>
 <span class="footnote">
-<?php echo $footnote; ?>
+<?php footnote(); ?>
 </span>
-<div class="post_content"><?php echo html_entity_decode($page->page_content); ?></div>
+<div class="post_content"><?php pageContent(); ?></div>
 </div>
-<?php echo get_date('j',$page->page_date);
