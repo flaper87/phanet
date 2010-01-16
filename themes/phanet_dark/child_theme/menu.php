1b03f4e6da5105ad6371b45097ee6be29879f657
incluye <jdtdj@fuse.net>
2010-01-16 15:22:16 -0500

    Fixed a few broken links (extra double quotes, causing them to not link anywhere)

--- themes/phanet_dark/child_theme/menu.php
+++ themes/phanet_dark/child_theme/menu.php
@@ -1,10 +1,10 @@
 <h2><p style="font-size: 16px; float:left;">
-	<img alt="pageicon" src=<?php echo $srv->getPath("themes/phanet_dark/styles/images/page.gif"); ?>" />
-  <a style="color: #999999 !important;" href=<?php echo $srv->getInstallRadix(); ?>">Home</a> 
+	<img alt="pageicon" src=<?php echo $srv->getPath("themes/phanet_dark/styles/images/page.gif"); ?> />
+  <a style="color: #999999 !important;" href="<?php echo $srv->getInstallRadix(); ?>">Home</a> 
 	
 <?php foreach ( $pages as $page ) { ?>
-	<img alt="pageicon" src=<?php echo $srv->getPath("themes/phanet_dark/styles/images/page.gif"); ?>" />
-	  <a style="color: #999999 !important;" href=<?php echo $srv->buildUrl("?static=".$page->id); ?>"><?php echo $page->page_title; ?></a> 
+	<img alt="pageicon" src=<?php echo $srv->getPath("themes/phanet_dark/styles/images/page.gif"); ?> />
+	  <a style="color: #999999 !important;" href="<?php echo $srv->buildUrl("?static=".$page->id); ?>"><?php echo $page->page_title; ?></a> 
 <?php }
 
 if ($_SESSION['userLogged'] && $_SESSION['adminLogged']) { ?>
@@ -13,9 +13,3 @@ if ($_SESSION['userLogged'] && $_SESSION['adminLogged']) { ?>
 <?php } ?>
 </div>
 <div id="posts">
-
-<?php if ($_GET["static"]) {
-	showPage();
-} else {
-	showPosts();
-} ?>
