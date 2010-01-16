6ae7d7d5c7de4696e6b3686c5250149bf8c5e791
incluye <jdtdj@fuse.net>
2010-01-16 16:40:31 -0500

    menu.php is now smaller thanks to themefunctions.inc.php

--- themes/phanet_dark/child_theme/menu.php
+++ themes/phanet_dark/child_theme/menu.php
@@ -1,15 +1,6 @@
 <h2><p style="font-size: 16px; float:left;">
-	<img alt="pageicon" src=<?php echo $srv->getPath("themes/phanet_dark/styles/images/page.gif"); ?> />
-  <a style="color: #999999 !important;" href="<?php echo $srv->getInstallRadix(); ?>">Home</a> 
-	
-<?php foreach ( $pages as $page ) { ?>
-	<img alt="pageicon" src=<?php echo $srv->getPath("themes/phanet_dark/styles/images/page.gif"); ?> />
-	  <a style="color: #999999 !important;" href="<?php echo $srv->buildUrl("?static=".$page->id); ?>"><?php echo $page->page_title; ?></a> 
-<?php }
+<?php listPages(array('icon'=>'true'));
 
-if ($_SESSION['userLogged'] && $_SESSION['adminLogged']) { ?>
-</p>
-	<a style="text-align:right;" href="'.$srv->buildUrl('?admin=').'"><span class="footnote">Admin Panel</span></a></h2>
-<?php } ?>
+adminLink(); ?>
 </div>
 <div id="posts">
