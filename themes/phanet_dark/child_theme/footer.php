ba45df1140398940569753fc28be707f32f09e32
incluye <jdtdj@fuse.net>
2010-01-16 16:39:48 -0500

    Spelling errors and made date() dynamic (before, every Phanet site was copyright 2008)

--- themes/phanet_dark/child_theme/footer.php
+++ themes/phanet_dark/child_theme/footer.php
@@ -1,5 +1,5 @@
-<div id="site_bottom">Phanet does not generate contents each post belongs to the author.<br/>
-© 2008 - <a href="http://www.phanet.net" >Phanet</a> is under GPLv2</div>
+<div id="site_bottom">Phanet does not generate content. Each post belongs to the author.<br/>
+©<?php echo date('Y'); ?> <?php siteInfo('name'); ?> - <a href="http://www.phanet.net" >Phanet</a> is under GPLv2</div>
 </div>
 </div>
 </body>
