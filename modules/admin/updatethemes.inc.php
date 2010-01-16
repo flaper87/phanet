1f7fd3f3c78aab198b0ec963f83da900e71943cf
incluye <jdtdj@fuse.net>
2010-01-16 13:07:29 -0500

    Phanet has a lib to handle database queries

--- modules/admin/updatethemes.inc.php
+++ modules/admin/updatethemes.inc.php
@@ -1,15 +1,13 @@
-<?php function listThemes() {
-	global $SETTINGS;
-$link = mysql_connect($SETTINGS['db_server'], $SETTINGS['db_user'], $SETTINGS['db_password']);
-if($link) {
-	if(mysql_select_db($SETTINGS['db_database'])) {
-		$query = "SELECT * FROM `".$SETTINGS['db_prefix']."settings` WHERE `keyid` = 'active_theme'";
-		$results = mysql_query($query);
-		$row = mysql_fetch_row($results);
-		$activetheme = $row[1];
-	}
-}
+<?php
+function listThemes() {
+global $ptdb;
+$ptdb->query("SELECT * FROM {settings} WHERE `keyid` = 'active_theme'");
+$row = $ptdb->fetchArray();
+$row2 = $row[0];
+$activetheme = $row2[1]; // UGLY
+
 	$installedThemes = array('');
+	
 	foreach (glob('themes/*') as $file) {
 		if(is_dir($file)) {
 			$newfile = str_replace("themes/","",$file);
@@ -22,4 +20,5 @@ if($link) {
 		if($theme == $activetheme) { $selected = ' selected'; }
 		echo "<option$selected value=\"".$theme."\">".$theme."</option>";
 	endforeach;
+	
 }
