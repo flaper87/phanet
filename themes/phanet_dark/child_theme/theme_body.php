e974eabcb86a081899dc230962f7ecbcc685bfb0
incluye <jdtdj@fuse.net>
2010-01-16 16:40:44 -0500

    theme_body.php is now useless and will shortly be removed.

--- themes/phanet_dark/child_theme/theme_body.php
+++ themes/phanet_dark/child_theme/theme_body.php
@@ -1,16 +1,1 @@
-<div id="menu">
-	<div class="title">
-		<h1><a href="<?php echo $srv->getInstallRadix(); ?>"><?php echo $stgs->getConf("sitename"); ?></a></h1>
-		<div class="description"><?php echo $stgs->getConf("sitedescription"); ?></div>
-	</div>
-	
-	<div class="logo">
-		<a href="<?php echo $srv->buildUrl('?feed='); ?>">
-		<img src="<?php echo $srv->getPath('themes/phanet_dark/styles/images/feed.png'); ?>" alt="feed_icon" class="favicon"/>
-		</a>
-	</div>
-</div>
-
-<div id="site">
-	<div id="content">
-	<div id="pagetitle">
+<?php // Nothing to see here.
