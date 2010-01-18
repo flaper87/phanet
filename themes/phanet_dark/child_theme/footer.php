ce08a21564dfdb24586e3878c2b7f2a7887dc676
incluye <jdtdj@fuse.net>
2010-01-18 13:36:07 -0500

    Don't need no steenking phanet_dark no more!

--- themes/phanet_dark/child_theme/footer.php
+++
@@ -1,6 +0,0 @@
-<div id="site_bottom">Phanet does not generate content. Each post belongs to the author.<br/>
-©<?php echo date('Y'); ?> <?php siteInfo('name'); ?> - <a href="http://www.phanet.net" >Phanet</a> is under GPLv2</div>
-</div>
-</div>
-</body>
-</html>
--- themes/phanet_dark/child_theme/menu.php
+++
@@ -1,6 +0,0 @@
-<h2><p style="font-size: 16px; float:left;">
-<?php listPages(array('icon'=>'true'));
-
-adminLink(); ?>
-</div>
-<div id="posts">
