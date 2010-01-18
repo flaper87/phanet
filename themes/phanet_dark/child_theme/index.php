cce0e627002199472b7fc915c154935b9637cd78
incluye <jdtdj@fuse.net>
2010-01-18 13:06:48 -0500

    Prettified index.php.

--- themes/phanet_dark/child_theme/index.php
+++ themes/phanet_dark/child_theme/index.php
@@ -1,14 +1,14 @@
 <div class="single_post">
 <div class="title_date">
-<p class="days">{%date:j}</p>
-<p class="month">{%date:M}</p>
+	<p class="days"><?php postDate('j'); ?></p>
+	<p class="month"><?php postDate('m'); ?></p>
 </div>
 <div class="title">
-<h4 id="blogname" name="{%blogName}">{%blog}</h4>
-<h4>{%title}</h4>
+<h4 id="blogname" name="<?php blogName(); ?>"><?php blogName(); ?></h4>
+<h4><?php postTitle(); ?></h4>
 </div>
 <span class="footnote">
-{%footnote}
+<?php footnote(); ?>
 </span>
-<div class="post_content">{%text}</div>
+<div class="post_content"><?php postContent(); ?></div>
 </div>
