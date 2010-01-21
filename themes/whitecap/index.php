6bef46cd14a5bb7347f964b2f4cbf968ac8e50c8
incluye <jdtdj@fuse.net>
2010-01-21 14:03:49 -0500

    Conditional tagging, we don't need a posts nav link or a trimmer if we're on a page

--- themes/whitecap/index.php
+++ themes/whitecap/index.php
@@ -1,13 +1,13 @@
 <?php getHeader(); ?>
 
 <div id="posts">
-	<div class="trimmer"><a class="collapse_expand_all">Trim</a></div> <!-- hides/shows content of all posts on this page -->
+	<?php if(!isPage()) : ?><div class="trimmer"><a class="collapse_expand_all">Trim</a></div> <!-- hides/shows content of all posts on this page -->
 	
-	<div class="paging"><b>Run to page: </b><?php createPaging(); ?></div>
+	<div class="paging"><b>Run to page: </b><?php createPaging(); ?></div><?php endif; ?>
 	
 		<?php getLoop(); // Iterates posts as specified by the limit. ?>
 		
-	<div class="paging"><b>Run to page: </b><?php createPaging(); ?></div>
+	<?php if(!isPage()) : ?><div class="paging"><b>Run to page: </b><?php createPaging(); ?></div><?php endif; ?>
 </div>
 
 <?php getSidebar(); ?>
