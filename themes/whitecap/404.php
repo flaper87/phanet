0f274e6f8f0ffde3c9bbe18900f44e7f6c8c6b29
incluye <jdtdj@fuse.net>
2010-01-21 14:22:40 -0500

    404: like index.php, but contains very little information.

--- themes/whitecap/404.php
+++ themes/whitecap/404.php
@@ -1,26 +1,58 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 
-<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en-US" lang="en-US">
+<html xmlns="http://www.w3.org/1999/xhtml">
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
-<title>Phanet: Page Not Found!!</title>
-<link rel="stylesheet" type="text/css" href="styles/style.css">
+<title><?php echo $stgs->getConf("sitename"); ?></title>
+
+<link rel="stylesheet" type="text/css" href="<?php echo $srv->getPath('themes/whitecap/styles/style.css'); ?>" />
+<link rel="stylesheet" type="text/css" href="<?php echo $srv->getPath('media/js/thickbox/ThickBox.css'); ?>" />
+<link rel="alternate" type="application/rss+xml" title="RSS 2.0" href="<?php echo $srv->buildUrl('?feed='); ?>"  />
+<link rel="search" type="application/opensearchdescription+xml" href="<?php echo $srv->buildUrl('?openSearch='); ?>" title="<?php echo $stgs->getConf("sitename"); ?>" />
+<link rel="icon" type="image/png" title="favicon" href="<?php echo $srv->getPath('media/phaneticon.png'); ?>"  />
+
+<script src="<?php echo $srv->getPath('media/js/jquery/jquery.js'); ?>" type="text/javascript"></script>
+<script src="<?php echo $srv->getPath('media/js/jquery/ui/ui.base.js'); ?>" type="text/javascript"></script>
+<script src="<?php echo $srv->getPath('media/js/jquery/ui/ui.sortable.js'); ?>" type="text/javascript"></script>
+<script src="<?php echo $srv->getPath('themes/whitecap/styles/collapsible.js'); ?>" type="text/javascript"></script>
+<script src="<?php echo $srv->getPath('media/js/thickbox/thickbox.js'); ?>" type="text/javascript"></script>
 </head>
 
 <body>
-<div id="site">
-<div id="content">
-<div id="posts">
-<div id="logo"><img src="media/phaneticon-big.png" width="80px"></div>
-<div class="title"><h1>Phanet</h1>
-<div class="subtitle">Just a feed agregator</div>
-</div>
-<div id="not_found">
-The page requested was not Found!!
-</div>	
-</div>
-<div id="site_bottom">(c)2008 - Phanet is under GPLv2</div>
-</div>
-</div>
-</body>
-</html>
+	<div id="header">
+		<div class="title">
+			<h1><a href="<?php siteInfo('url'); ?>"><?php siteInfo('name'); ?></a></h1>
+			<div class="description"><?php siteInfo('description'); ?></div>
+		</div>
+		<div class="nav">
+			<ul>
+		<?php listPages(); adminLink(); ?>
+			</ul>
+		</div>
+	</div>
+	</div>
+	<div id="content">
+		<div id="posts">
+
+				<div class="single_post">
+					<div class="post_title">
+						<h3><a>404 :: Not Found</a></h3>
+					</div>
+					<div class="post_content">
+						<p>Sorry, but we couldn't find that page!</p>
+					</div>
+				</div>
+
+		</div>
+		<div class="sidebar">
+
+		<?php sidebarMessages(); // This is always needed with dynamic and non-dynamic sidebars
+		if (widgetized()): loadSidebar(); endif; // show the dynamic sidebar if it's set up
+		?>
+
+		</div>
+		</div>
+		<div id="site_bottom">Phanet does not generate content. Each post belongs to the author.<br/>
+		©<?php echo date('Y'); ?> "<?php siteInfo('name'); ?>" - <a href="http://www.phanet.net" >Phanet</a> is under GPLv2</div>
+		</body>
+		</html>
