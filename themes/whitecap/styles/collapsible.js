c5551e97a56cd062f1fe0970de636bc9554805b0
incluye <jdtdj@fuse.net>
2010-01-20 23:49:34 -0500

    Easy collapsing and uncollapsing of all the posts on the page, inspired by K2

--- themes/whitecap/styles/collapsible.js
+++ themes/whitecap/styles/collapsible.js
@@ -2,32 +2,32 @@ $( document ).ready(function(){
 	
 	$('div.block div.text', this).each(function() { 
 		if ( $(this).attr("id") != "notToggle" ) {
-			$( "div.text", this.parentNode ).slideToggle("instant");
-			$( "div.title", this.parentNode ).css("cursor", "pointer");
+			$( "div.text", this.parentNode, "div.meta" ).slideToggle("instant");
+			$( "div.title", this.parentNode, "div.meta" ).css("cursor", "pointer");
 		}
 	})
-	
-	$( 'div.collapse_expand_all', this ).css("cursor", "pointer");
-	
-	$( 'div.single_post div.title_date', this ).each(function() {
-			$( this ).css("cursor", "pointer");
-	})
-	
+
 	$( 'div.block div.title', this ).click(function() {
 			if ( $(this).attr("id") != "notToggle" ) {
-				$( "div.text", this.parentNode ).slideToggle("slow");	
+				$( "div.text", this.parentNode, "div.meta" ).slideToggle("slow");	
 			}
 		})
 		
-	$( 'div.single_post div.title_date', this ).click(function() {
-			$( "div.post_content", this.parentNode ).slideToggle("slow");	
-		})
-		
-	$( 'div.collapse_expand_all', this ).click(function() {
-			if ( $(this).text() == "Collapse All")
-				$( this ).html("Expand All")
+	$('.collapse_expand_all').live("click", function() {
+			if ( $(this).text() == "Trim")
+				$( this ).html("Untrim")
 			else
-				$( this ).html("Collapse All")
-			$( "div.post_content", $(document) ).slideToggle("slow");	
+				$( this ).html("Trim")
+		$( "div.post_content").addClass("hidden");
+		$("div.single_post:odd").addClass("odd");
+		$("div#posts").addClass("toggled");
+		$(this).addClass('expand_all');
+			
+	$('.expand_all').live("click", function() {
+		$( "div.post_content").removeClass("hidden");
+		$(this).removeClass('expand_all');
+		$("div#posts").removeClass("toggled");
+		$("div.single_post:odd").removeClass("odd");
+	})
 	})
 });
