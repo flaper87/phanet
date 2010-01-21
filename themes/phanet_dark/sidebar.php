0e1b73e0e62017f066c3ad061e8afd48acdd4db4
incluye <jdtdj@fuse.net>
2010-01-21 14:24:28 -0500

    phanet_dark, you aren't welcome here.

--- themes/phanet_dark/sidebar.php
+++
@@ -1,7 +0,0 @@
-<div class="sidebar_left">
-
-<?php sidebarMessages(); // This is always needed with dynamic and non-dynamic sidebars
-if (widgetized()): echo $wgts->showSidebar(); endif; // show the dynamic sidebar if it's set up
-?>
-	
-</div>
