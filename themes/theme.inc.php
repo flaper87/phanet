706e6c244d8c11016ab77ff071effa964acdf1cb
incluye <jdtdj@fuse.net>
2010-01-20 23:48:25 -0500

    Removed a ton of extra code. theme.inc.php relies only on index.php now, which can then call other files. Neat eh?

--- themes/theme.inc.php
+++ themes/theme.inc.php
@@ -8,7 +8,7 @@ include_once ('bottom.inc.php');
 require_once('include/themefunctions.inc.php');
 
 function themeTemplate($id=''){
-	global $page, $footnote, $activetheme;
+	global $page, $footnote, $activetheme, $posts, $wgts, $srv, $stgs, $SESSION;
 	switch($id){
 		case 'post':
 			$output[] = include($activetheme.'/index.php');
@@ -17,21 +17,15 @@ function themeTemplate($id=''){
 			$output[] = include($activetheme.'/page.php');
 			break;
 	}
-	return join("\n", $output);
+	return $output[1];
 }
 
 function themeRender($params = array()){
 	
 	$name = $params[0]; 
 	array_shift($params); 
-
-	 	loadThemeHeader();
 		
 		showThemeBody();
-		
-		showThemeSidebar();
-		
-		showThemeBottom();
 }
 
 function showThemeBody() {
@@ -39,81 +33,19 @@ function showThemeBody() {
 		
 		$pages = getPages();
 		
-		$menu[] = require($activetheme.'/menu.php');
-		
-		if ($_SESSION['userLogged'] && $_SESSION['adminLogged']) {
-			$output[] = '<h2><p style="font-size: 16px; float:left;">'.join("\n",$menu).'</p>';
-		
-			$output[] = '<a style="text-align:right;" href="'.$srv->buildUrl('?admin=').'"><span class="footnote">Admin Panel</span></a></h2>';
-		} else {
-			$output[] = '<h2 style="font-size: 16px; text-align:left;">'.join("\n",$menu).'</h2>';
-		}
-		
-		$output[] = '</div>';
-		$output[] = '<div id="posts">';
-		
         if ($_GET["static"]) {
 			$output[] = showPage();
 		} else {
 			$output[] = showPosts();
 		}
 
-	echo '</div>';
+	// echo '</div>';
 }
 
 function showPosts() {
-	
 	global $footnote, $post;
-
-		//Not supported yet.. I'm working on it
-	if (!isset($_POST["searchPosts"]) && !isset($_POST["advanceSearch"]) && !isset($_GET["openSearch"])) {
-		$output[] = '<div id="paging">';
-		$output[] = '<b>Run to page: </b>'.createPaging();
-		$output[] = '</div>';
-	}
-	
-	$output[] = '<div class="collapse_expand_all">Collapse All</div>';
-
-	$posts = getPosts(-1);
-
-	if (!$posts) {
-	 	echo 'No posts were found!';
-	}
-
-	$i = 0;
-				 
-	foreach( $posts as $post){
-		if ( $i == 20 ) break;
 		
-		$author='';
-		
-		$title = $post->title;
-		
-		$blog = 'Source: <a href="'. $post->url .'">' . $post->name . '</a> ';
-		$beforelink = '';
-		$afterlink = '';
-	    
-		if ($post->link) {
-			$beforelink='<a href="'.$post->link.'" target="_blank">';
-	        $afterlink='</a>';
-		}
-		
-		$author = ($post->nickname == "anonymous" or !$post->nickname)?$post->owner_nick:$post->nickname;
-		
-		$footnote = 'Written by: '. $beforelink . $author . $afterlink .' on '.get_date('r',$post->date);
-
-		$words = 0;
-
-		if ($words != 0 ) {
-			$a = explode(' ', $text);
-			array_splice( $a, $words);
-			$text = implode(' ', $a) . '   <a href="'.$post->link.'">[... Read More ...]</a>';
-		} 
-		
-		$output[] = themeTemplate('post');
-		$i++;
-	}
-	echo $output[1];
+	echo themeTemplate('post');
 }
 
 function showPage() {
