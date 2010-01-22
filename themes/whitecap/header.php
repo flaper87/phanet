b5b1df2bcc646f4efbe3336e7b34fac9fd026ef5
incluye <jdtdj@fuse.net>
2010-01-21 19:01:09 -0500

    Legible header.php, uses a few new functions.

--- themes/whitecap/header.php
+++ themes/whitecap/header.php
@@ -3,19 +3,18 @@
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
-<title><?php echo $stgs->getConf("sitename"); ?></title>
+<title><?php siteInfo('name'); ?></title>
 
-<link rel="stylesheet" type="text/css" href="<?php echo $srv->getPath('themes/whitecap/styles/style.css'); ?>" />
-<link rel="stylesheet" type="text/css" href="<?php echo $srv->getPath('media/js/thickbox/ThickBox.css'); ?>" />
-<link rel="alternate" type="application/rss+xml" title="RSS 2.0" href="<?php echo $srv->buildUrl('?feed='); ?>"  />
-<link rel="search" type="application/opensearchdescription+xml" href="<?php echo $srv->buildUrl('?openSearch='); ?>" title="<?php echo $stgs->getConf("sitename"); ?>" />
-<link rel="icon" type="image/png" title="favicon" href="<?php echo $srv->getPath('media/phaneticon.png'); ?>"  />
+<link rel="stylesheet" type="text/css" href="<?php siteInfo('theme_path') ?>/style.css" />
+<link rel="alternate" type="application/rss+xml" title="RSS 2.0" href="<?php feedURL(); ?>"  />
+<link rel="search" type="application/opensearchdescription+xml" href="?openSearch=" title="<?php siteInfo('name'); ?>" />
+<link rel="icon" type="image/png" title="favicon" href="media/phaneticon.png"  />
 
-<script src="<?php echo $srv->getPath('media/js/jquery/jquery.js'); ?>" type="text/javascript"></script>
-<script src="<?php echo $srv->getPath('media/js/jquery/ui/ui.base.js'); ?>" type="text/javascript"></script>
-<script src="<?php echo $srv->getPath('media/js/jquery/ui/ui.sortable.js'); ?>" type="text/javascript"></script>
-<script src="<?php echo $srv->getPath('themes/whitecap/styles/collapsible.js'); ?>" type="text/javascript"></script>
-<script src="<?php echo $srv->getPath('media/js/thickbox/thickbox.js'); ?>" type="text/javascript"></script>
+<?php loadScript('jquery'); ?>
+<?php loadScript('jqueryUIbase'); ?>
+<?php loadScript('jqueryUIsortable'); ?>
+<?php loadScript('thickbox'); ?>
+<script src="<?php siteInfo('theme_path') ?>/styles/collapsible.js" type="text/javascript"></script>
 </head>
 
 <body>
