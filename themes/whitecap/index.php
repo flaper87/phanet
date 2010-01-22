b0faf78663f828cbf3cc503cbdccb41403505a36
incluye <jdtdj@fuse.net>
2010-01-21 19:18:15 -0500

    Show "trim" link if it isn't a 404 page

--- themes/whitecap/index.php
+++ themes/whitecap/index.php
@@ -1,13 +1,13 @@
 <?php getHeader(); ?>
 
 <div id="posts">
-	<?php if(!isPage()) : ?><div class="trimmer"><a class="collapse_expand_all">Trim</a></div> <!-- hides/shows content of all posts on this page -->
+<?php if(!is404() && !isPage()) : ?><div class="trimmer"><a class="collapse_expand_all">Trim</a></div> <!-- hides/shows content of all posts on this page -->
 	
 	<div class="paging"><b>Run to page: </b><?php createPaging(); ?></div><?php endif; ?>
 	
 		<?php getLoop(); // Iterates posts as specified by the limit. ?>
 		
-	<?php if(!isPage()) : ?><div class="paging"><b>Run to page: </b><?php createPaging(); ?></div><?php endif; ?>
+	<?php if(!is404() && !isPage()) : ?><div class="paging"><b>Run to page: </b><?php createPaging(); ?></div><?php endif; ?>
 </div>
 
 <?php getSidebar(); ?>
